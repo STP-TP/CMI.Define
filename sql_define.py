273834a9cf921cbd775af3fce98e36c2d37c4da7
jooss287 <jooss287@gmail.com>
2020-06-11 21:48:24 +0900

    [refector] Add, refect common define names

--- sql_define.py
+++ sql_define.py
@@ -1,4 +1,86 @@
 
+################################################
+# DATABASE TABLE AND FIELD NAME
+# ATTRIBUTE_TABLE
+ATTRIBUTE_TABLE = "attributeTbl"
+ATTRIBUTE_ID = "attributeId"
+ATTRIBUTE_NAME = "attributeName"
+ATTRIBUTE_EXPLAIN = "attributeExplain"
+
+# CHARACTER_TABLE
+CHARACTER_TABLE = "characterTbl"
+CHARACTER_ID = "characterId"
+CHARACTER_NAME = "characterName"
+
+# ITEM_TABLE
+ITEM_TABLE = "itemTbl"
+ITEM_NAME = "itemName"
+SLOT_CODE = "slotCode"
+SLOT_NAME = "slotName"
+RARITY_CODE = "rarityCode"
+RARITY_NAME = "rarityName"
+EQUIP_SLOT_CODE = "equipSlotCode"
+EQUIP_SLOT_NAME = "equipSlotName"
+SEASON_CODE = "seasonCode"
+SEASON_NAME = "seasonName"
+ITEM_EXPLAIN = "explain"
+ITEM_EXPLAIN_DETAIL = "explainDetail"
+
+# MATCH_TABLE
+MATCH_TABLE = "matchTbl"
+DATE = "date"
+MATCH_ID = "matchId"
+MAP_ID = "mapId"
+MAP_NAME = "mapName"
+ITEM_ID = "itemId"
+GAME_TYPE_ID = "gameTypeId"
+
+# MATCH_DETAIL_TABLE
+MATCH_DETAIL_TABLE = "match_detailTbl"
+RESULT = "result"
+RANDOM = "random"
+PARTY_USER_COUNT = "partyUserCount"
+LEVEL = "level"
+KILL_COUNT = "killCount"
+DEATH_COUNT = "deathCount"
+ASSIST_COUNT = "assistCount"
+ATTACK_POINT = "attackPoint"
+DAMAGE_POINT = "damagePoint"
+BATTLE_POINT = "battlePoint"
+SIGHT_POINT = "sightPoint"
+PLAY_TIME = "playTime"
+ATTRIBUTE_ID_LV1 = "attributeId_lv1"
+ATTRIBUTE_ID_LV2 = "attributeId_lv2"
+ATTRIBUTE_ID_LV3 = "attributeId_lv3"
+ITEM_101 = "item_101"
+ITEM_102 = "item_102"
+ITEM_103 = "item_103"
+ITEM_104 = "item_104"
+ITEM_105 = "item_105"
+ITEM_106 = "item_106"
+ITEM_107 = "item_107"
+ITEM_202 = "item_202"
+ITEM_203 = "item_203"
+ITEM_204 = "item_204"
+ITEM_205 = "item_205"
+ITEM_301 = "item_301"
+ITEM_302 = "item_302"
+ITEM_303 = "item_303"
+ITEM_304 = "item_304"
+ITEM_305 = "item_305"
+
+# POSITION_TABLE
+POSITION_TABLE = "positionTbl"
+POSITION_NAME = "positionName"
+POSITION_EXPLAIN = "explain"
+
+# SEARCH_DATE_TABLE
+SEARCH_DATE_TABLE = "search_dateTbl"
+PAST_DATE = "past_date"
+RECENT_DATE = "recent_date"
+
+# USER_TABLE
+USER_TABLE = "userTbl"
 PLAYER_ID = "playerId"
 NICKNAME = "nickname"
 GRADE = "grade"
@@ -12,46 +94,21 @@ RATING_STOP = "ratingStop"
 NORMAL_WIN = "normalWin"
 NORMAL_LOSE = "normalLose"
 NORMAL_STOP = "normalStop"
+################################################
+# ETC
 WIN_COUNT = "winCount"
 LOSE_COUNT = "loseCount"
 STOP_COUNT = "stopCount"
-MATCH_ID = "matchId"
-RESULT = "result"
-RANDOM = "random"
-PARTY_USER_COUNT = "partyUserCount"
-CHARACTER_ID = "characterId"
-LEVEL = "level"
-KILL_COUNT = "killCount"
-DEATH_COUNT = "deathCount"
-ASSIST_COUNT = "assistCount"
-ATTACK_POINT = "attackPoint"
-DAMAGE_POINT = "damagePoint"
-BATTLE_POINT = "battlePoint"
-SIGHT_POINT = "sightPoint"
-PLAY_TIME = "playTime"
 POSITION = "position"
 ATTRIBUTE = "attribute"
 ITEMS = "items"
-DATE = "date"
-GAME_TYPE_ID = "gameTypeId"
 PLAYERS = "players"
-POSITION_NAME = "positionName"
-POSITION_EXPLAIN = "explain"
-MAP_ID = "mapId"
-MAP_NAME = "mapName"
-ITEM_ID = "itemId"
-ITEM_NAME = "itemName"
-SLOT_CODE = "slotCode"
-SLOT_NAME = "slotName"
-RARITY_CODE = "rarityCode"
-RARITY_NAME = "rarityName"
-EQUIP_SLOT_CODE = "equipSlotCode"
-EQUIP_SLOT_NAME = "equipSlotName"
-SEASON_CODE = "seasonCode"
-SEASON_NAME = "seasonName"
-ITEM_EXPLAIN = "explain"
-ITEM_EXPLAIN_DETAIL = "explainDetail"
-ATTRIBUTE_ID = "attributeId"
-ATTRIBUTE_NAME = "attributeName"
-ATTRIBUTE_EXPLAIN = "attributeExplain"
-CHARACTER_NAME = "characterName"
+
+
+
+
+
+
+
+
+
